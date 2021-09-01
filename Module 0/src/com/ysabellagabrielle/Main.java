6ecdbf16ef2c83088c3370671234c404eaf74d01
ysabellaperf <ysabellagabrielle.perfecto@benilde.edu.ph>
2021-09-01 23:43:13 +0800

    Added Module 0 (for Testing Purposes)

---
+++ Module 0/src/com/ysabellagabrielle/Main.java
@@ -0,0 +1,17 @@
+package com.ysabellagabrielle;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name + " uwu" );
+
+    }
+}
---
+++ src/com/Ysa/Main.java
@@ -0,0 +1,10 @@
+package com.Ysa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // prints out hello world
+        System.out.println("Hellow World");
+    }
+}
